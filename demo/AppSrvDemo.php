1b7a43456b60f3ec300f5bef2f598aeb2a8ebf11
bostjan <bostjan@a2o.si>
2009-08-18 01:11:51 +0200

    Fixed demo

--- demo/AppSrvDemo.php
+++ demo/AppSrvDemo.php
@@ -3,4 +3,8 @@
 class AppSrvDemo extends A2o_AppSrv
 {
 
+    protected function worker_handleRequest ()
+    {
+	$this->_wp_serverResponse = $this->_wp_clientRequest . "\nI do work, you know, copying the input to the output...";
+    }
 }
